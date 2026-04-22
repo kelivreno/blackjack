9a5cd833e6481335a5a6b8c4815d1f2f4ed9242d
Kelangit Hakim <90366335+kelivreno@users.noreply.github.com>
2026-04-21 23:36:50 -0700

    add game logic to evaluate card sum and display messages

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ let firstCard = 6
 let secondCard = 7
 
 let sum = firstCard+secondCard
+
+if (sum<=20) {
+    console.log("Do you want to draw a new card? 🙂")
+} else if (sum===21) {
+    console.log("Wohoo! You've got Blackjack! 🥳")
+} else {
+    console.log("You're out of the game! 😭")
+}
