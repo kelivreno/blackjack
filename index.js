d806a3264876da83dd862d457ef360b4fc46b623
Kelangit Hakim <90366335+kelivreno@users.noreply.github.com>
2026-04-22 01:46:30 -0700

    refactor: store messages in a variable for better readability

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 let firstCard = 6
 let secondCard = 7
 let sum = firstCard+secondCard
-
 let hasBlackJack = false
 let isAlive = true // to see if they're still alive in the game
+let message = ""
 
 if (sum<=20) {
-    console.log("Do you want to draw a new card? 🙂")
+    message = "Do you want to draw a new card? 🙂"
 } else if (sum===21) {
-    console.log("Wohoo! You've got Blackjack! 🥳")
+    message = "Wohoo! You've got Blackjack! 🥳"
     hasBlackJack = true
 } else {
-    console.log("You're out of the game! 😭")
+    message = "You're out of the game! 😭"
     isAlive = false 
 }
+
+console.log(message)
