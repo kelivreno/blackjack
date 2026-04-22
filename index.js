555642e17266216ccc69dca5d13fa90d2e1d0ae3
Kelangit Hakim <90366335+kelivreno@users.noreply.github.com>
2026-04-22 01:28:25 -0700

    refactor: add game state variables for Blackjack logic

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 let firstCard = 6
 let secondCard = 7
-
 let sum = firstCard+secondCard
+
+let hasBlackJack = false
+let isAlive = true // to see if they're still alive in the game
 
 if (sum<=20) {
     console.log("Do you want to draw a new card? 🙂")
 } else if (sum===21) {
     console.log("Wohoo! You've got Blackjack! 🥳")
+    hasBlackJack = true
 } else {
     console.log("You're out of the game! 😭")
+    isAlive = false 
 }
