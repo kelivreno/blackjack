55295f1705ee4af4a131a370f8cbb8aff3f62e07
Kelangit Hakim <90366335+kelivreno@users.noreply.github.com>
2026-04-22 02:24:50 -0700

    refactor: improve button placement and game logic structure

--- index.js
+++ index.js
@@ -5,14 +5,16 @@ let hasBlackJack = false
 let isAlive = true // to see if they're still alive in the game
 let message = ""
 
-if (sum<=20) {
+function startGame() {
+
+if (sum <= 20) {
     message = "Do you want to draw a new card? 🙂"
-} else if (sum===21) {
+} else if (sum === 21) {
     message = "Wohoo! You've got Blackjack! 🥳"
     hasBlackJack = true
 } else {
     message = "You're out of the game! 😭"
-    isAlive = false 
+    isAlive = false
 }
-
 console.log(message)
+}
