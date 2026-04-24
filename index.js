e06bd53e55f3ebacf7b2f2930071056a37f2ff64
Kelangit Hakim <90366335+kelivreno@users.noreply.github.com>
2026-04-23 21:49:17 -0700

    refactor: add id to cards display for improved element selection

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ let isAlive = true // to see if they're still alive in the game
 let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
 // let sumEl = document.querySelector("#sum-el") ("body") //need hastag to specify it is an id
 // if it's a class, use a ".", so it will be (".sum-el")
 function startGame() {
 sumEl.textContent="Sum: " + sum
+cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
 if (sum <= 20) {
     message = "Do you want to draw a new card?"
 } else if (sum === 21) {
